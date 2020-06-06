7395e7e8fc19c1dfb44de207f360e8e15b407fe2
pwwang <pwwang@pwwang.com>
2020-06-05 19:43:40 -0500

    Adopt latest cmdy

--- tests/test_require.py
+++ tests/test_require.py
@@ -5,13 +5,16 @@ from os import environ
 from pathlib import Path
 from pyppl.utils import chmod_x
 
-cmd = cmdy.pyppl.bake(Path(__file__).parent.joinpath('pyppl'), 'require', _exe = sys.executable, _raise = False)
+@pytest.fixture
+def pyppl():
+	return cmdy.pyppl(_exe=Path(__file__).parent.joinpath('pyppl'),
+				 _sub=True, _raise=False)
 procs = Path(__file__).parent.joinpath('procs.py')
 pipeline = Path(__file__).parent.joinpath('pipeline.py')
 none = Path(__file__).parent.joinpath('none.py')
 
-def test_procs():
-	err = cmd(p = procs).stderr
+def test_procs(pyppl):
+	err = pyppl.require(p = procs).stderr
 	assert 'No pipelines found' in err
 	assert 'But found processes:' in err
 	assert "Proc(name='pProcess1.notag')" in err
@@ -22,9 +25,9 @@ def test_procs():
 	assert '- tool2: condition (when) not met, skip.' in err
 	assert 'tool2: PASSED' in err
 
-def test_script_in_path():
+def test_script_in_path(pyppl):
 	chmod_x(procs)
-	err = cmd(p = 'procs.py', install = True,
+	err = pyppl.require(p = 'procs.py', install = True,
 		_env = {'PATH': procs.parent.resolve().as_posix() + ':' + environ['PATH']}).stderr
 	assert 'No pipelines found' in err
 	assert 'But found processes:' in err
@@ -34,22 +37,21 @@ def test_script_in_path():
 	assert '- tool2: FAILED' in err
 	assert 'tool1: No `install` found in the annotation.' in err
 
-def test_pipeline_not_found():
-	err = cmd(p = '__nonexist/_file__').stderr
+def test_pipeline_not_found(pyppl):
+	err = pyppl.require(p = '__nonexist/_file__').stderr
 	assert 'Pipeline script does not exist:' in err
 
-def test_none():
-	err = cmd(p = none).stderr
+def test_none(pyppl):
+	err = pyppl.require(p = none).stderr
 	assert 'No pipelines nor processes found.' in err
 
-def test_pipeline():
-	c = cmd(p = pipeline)
-	print(c.cmd)
+def test_pipeline(pyppl):
+	c = pyppl.require(p = pipeline)
 	assert 'Pipeline was prevented from running by pyppl_require.' in c.stderr
 	# unrelated procs are not loaded.
 	assert 'pProcessNoValid' not in c.stderr
 
-def test_noclimode():
+def test_noclimode(pyppl):
 	"""Not in cli mode, pipeline should be running"""
 	c = cmdy.python(pipeline, _exe = sys.executable, _raise = False)
 	assert 'Pipeline was prevented from running by pyppl_require.' not in c.stderr
